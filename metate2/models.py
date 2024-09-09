d02233ebdacf68ccad960fd0a9ebaea8078a7484
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-09-10 04:11:57 +0900

    models done

--- metate2/models.py
+++ metate2/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+class SeisakuNumberModel(models.Model):
+    seisaku_number = models.CharField(max_length=4, primary_key=True)   # AA98
+    part_number = models.CharField(max_length=30)                       # PFR5B11
+    po = models.CharField(max_length=4)                                 # P10
+    part_length = models.PositiveIntegerField()                         # 73.83
+    gata_3 = models.BooleanField()                                      # True    
+    gata_5 = models.BooleanField()                                      # False   
+
+
+    def __str__(self):
+        return self.seisaku_number
+
+class FuranjiModel(models.Model):
+    furanji = models.PositiveIntegerField()
+    seisaku_number = models.ForeignKey('SeisakuNumberModel', on_delete=models.CASCADE)
+    gata = models.CharField(max_length=2)
+    condition = models.CharField(max_length=20)
+    pedra = models.ForeignKey('ListaPedraModel', on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.furanji
+    
+class PedraTypeModel(models.Model):
+    type = models.CharField(max_length=20, primary_key=True)
+    haba = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.type
+
+class KumikaeOperationsModel(models.Model):
+    operation = models.CharField(max_length=20, primary_key=True)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.operation
+
+class KumikaeModel(models.Model):
+    date = models.DateTimeField()
+    #pedra_number = models.ForeignKey('ListaPedraModel', on_delete=models.CASCADE)
+    flange_montado = models.ForeignKey('FuranjiModel', on_delete=models.CASCADE)
+    #code_montado = models.CharField(max_length=10)
+    #hinban_montado = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    usada = models.PositiveIntegerField()  # Used quantity
+    descarte = models.PositiveIntegerField()  # Discarded quantity
+    flange_desmontado = models.ForeignKey('FuranjiModel', on_delete=models.CASCADE)  # Dismantled flange
+    #codigo_desmontado = models.CharField(max_length=10)  # Dismantled code
+    #hinban_desmontado = models.CharField(max_length=20)  # Dismantled hinban
+    feito = models.ForeignKey('KumikaeOperationsModel', on_delete=models.CASCADE)  # Operation done
+
+    def __str__(self):
+        return f"{self.date} - Name {self.name}"
+    
+class ListaPedraModel(models.Model):
+    pedra_number = models.AutoField(primary_key=True)                               # 1234
+    pedra_type = models.ForeignKey('PedraTypeModel', on_delete=models.CASCADE)      # Nitolex, Noritake
+    pedra_haba = models.PositiveIntegerField()                                      # 65, 90, 95, 100, 115         
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.pedra_number)
