33cbd1897b83f06b22c97315549c6c71522fa37a
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-09-10 04:21:02 +0900

    3

--- metate2/admin.py
+++ metate2/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+    SeisakuNumberModel,
+    FuranjiModel,
+    PedraTypeModel,
+    KumikaeOperationsModel,
+    KumikaeModel,
+    ListaPedraModel
+)
 
-# Register your models here.
+# Register your models here
+admin.site.register(SeisakuNumberModel)
+admin.site.register(FuranjiModel)
+admin.site.register(PedraTypeModel)
+admin.site.register(KumikaeOperationsModel)
+admin.site.register(KumikaeModel)
+admin.site.register(ListaPedraModel)
